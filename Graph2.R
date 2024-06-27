714200b8dcca268d92c1ef6d21f8703ad7515e8e
Jose Arturo Remartinez Gil <17684880+GAYARRI@users.noreply.github.com>
2024-06-27 12:00:30 +0200

    Delete Graph2.R

--- Graph2.R
+++
@@ -1,5 +0,0 @@
-png(file = "plot1.png", ,width=480,height=480)
-plot(SubEnergy$Global_active_power/500,col=1,type="l",xlab="",ylab ="Global Active Power (kilowatts)",axes=FALSE)
-axis(1,c(1,1440,2881),c("Thu","Fri","Thu"))
-axis(2,c(0.148,2.1,3.6,5.5),c(0,2,4,6))
-dev.off()
